b0c104acfd4c1e49de6417dedeb212f27ec8034c
Arnold Jabea <aroljabea71@gmail.com>
2024-11-06 05:12:14 +0100

    New_events

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import { createRoot } from "react-dom/client";
-// eslint-disable-next-line import/no-unresolved
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
-
-function App() {
-  return(
-  <div>
-    <h1>Adopt me!</h1>
-    <SearchParams />
-
-  </div>
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
   );
 };
+
 const container = document.getElementById("root");
-const root =createRoot(container);
-root.render(<App/>);
+const root = createRoot(container);
+root.render(<App />);
--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,35 +1,12 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
 
-const localcache ={};
+
 
 export default function useBreedList(animal) {
-    const [breedList, setBreedList] = useState([]);
-    const [status , setStatus] = useState("unloaded");
 
-    useEffect(() => {
-        
-        if(!animal){
-            setBreedList([]);
-        }else if(localcache[animal]){
-            setBreedList(localcache[animal]);
-           
-        } else{
-            requestBreedList();
-        }
-        async function requestBreedList(){
-            setBreedList([]);
-            setStatus("loading");
-
-            const res = await fetch(
-                `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-            );
-
-            const json = await res.json();
-            localcache[animal] = json.breeds || [];
-            setBreedList(localcache[animal]);
-            setStatus("loaded");
-        }
-    } , [animal]);
-    return [breedList, status];
+    const results = useQuery(["breeds", animal], fetchBreedList);
+ 
+    return [results?.data?.breeds ??[],results.status];
 
 }
